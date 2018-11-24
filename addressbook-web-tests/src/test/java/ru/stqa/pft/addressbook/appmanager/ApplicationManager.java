dccaf81f90323379f31b5909188f9be3cedd6cb5
Adam Jackowski <adamus95@vp.pl>
2018-11-24 17:21:34 +0100

    3_explorers_edge_included

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -4,8 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
@@ -23,17 +23,17 @@ public class ApplicationManager {
   }
 
   public void init() {
-    if(browser == BrowserType.FIREFOX)
+    if(browser.equals(BrowserType.FIREFOX))
     {
       wd = new FirefoxDriver();
     }
-    else if (browser == BrowserType.CHROME)
+    else if (browser.equals(BrowserType.CHROME))
     {
       wd = new ChromeDriver();
     }
-    else if (browser == BrowserType.IE)
+    else if (browser.equals(BrowserType.EDGE))
     {
-      wd = new InternetExplorerDriver();
+      wd = new EdgeDriver();
     }
 
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
