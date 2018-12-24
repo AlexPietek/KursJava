b9638bec70aee3d64e0e540a9785828539d64510
Adam Jackowski <adamus95@vp.pl>
2018-12-24 16:57:11 +0100

    data_generators

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,32 +1,67 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation() {
+  @DataProvider
+  public Iterator<Object[]> validContactsFromXml() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File(("src/test/resources/contacts.xml"))));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null) {
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xStream = new XStream();
+    xStream.processAnnotations(ContactData.class);
+    List<ContactData> contact = (List<ContactData>) xStream.fromXML(xml);
+
+    return contact.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+  }
+
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File(("src/test/resources/contacts.json"))));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+
+    Gson gson = new Gson();
+    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {
+    }.getType());
+
+    return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validContactsFromJson")
+  public void testContactCreation(ContactData contact) {
     app.goTo().gotoHomePage();
     Contacts before = app.contact().all();
-    app.contact().initContactCreation();
-    ContactData contact = new ContactData().withFirstName("Adam")
-            .withLastName("Jackowski")
-            .withAddress("Warszawa")
-            .withMailAdress1("myMail1")
-            .withMailAdress2("myMail2")
-            .withMailAdress3("myMail3")
-            .withHomePhone("111")
-            .withMobilePhone("222")
-            .withWorkPhone("333")
-            .withGroup("test1");
     app.contact().create(contact);
+    app.goTo().gotoHomePage();
+    assertThat(app.contact().count(), equalTo(before.size() + 1));
     Contacts after = app.contact().all();
     assertThat(after, equalTo(before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
-    assertThat(after.size(), equalTo(before.size() + 1));
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,27 +1,72 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() {
-    app.goTo().groupPage();
-    Groups before = app.group().all();
-    GroupData group = new GroupData().withName("test2");
-    app.group().create(group);
-    assertThat(app.group().count(), equalTo(before.size() + 1));
-    Groups after = app.group().all();
-    assertThat(after, equalTo(
-            before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+  @DataProvider
+  public Iterator<Object[]> validGroupsFromXml() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File(("src/test/resources/groups.xml"))));
+    String xml = "";
+    String line = reader.readLine();
+    while(line != null)
+    {
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xStream = new XStream();
+    xStream.processAnnotations(GroupData.class);
+    List<GroupData> group = (List<GroupData>) xStream.fromXML(xml);
+
+    return group.stream().map((g)-> new Object[]{g}).collect(Collectors.toList()).iterator();
   }
 
-  @Test
+  @DataProvider
+  public Iterator<Object[]> validGroupsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File(("src/test/resources/groups.json"))));
+    String json = "";
+    String line = reader.readLine();
+    while(line != null)
+    {
+      json += line;
+      line = reader.readLine();
+    }
+
+    Gson gson = new Gson();
+    List<GroupData> groups = gson.fromJson(json,new TypeToken<List<GroupData>>(){}.getType());
+
+    return groups.stream().map((g)-> new Object[]{g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validGroupsFromJson")
+  public void testGroupCreation(GroupData group) {
+      app.goTo().groupPage();
+      Groups before = app.group().all();
+      app.group().create(group);
+      assertThat(app.group().count(), equalTo(before.size() + 1));
+      Groups after = app.group().all();
+      assertThat(after, equalTo(
+              before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+    }
+
+  @Test(enabled = false)
   public void testBadGroupCreation() {
     app.goTo().groupPage();
     Groups before = app.group().all();
@@ -29,7 +74,6 @@ public class GroupCreationTests extends TestBase {
     app.group().create(group);
     Groups after = app.group().all();
     assertThat(app.group().count(), equalTo(before.size()));
-    assertThat(after, equalTo(
-            before));
+    assertThat(after, equalTo(before));
   }
 }
